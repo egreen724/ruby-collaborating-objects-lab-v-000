c328f5343a56747c2103f7005863964b803c9f80
Erin Green <erin.y.green@gmail.com>
2018-10-18 15:13:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+require "pry"
+
+>>>>>>> fa915f8838ddf6833bf0c3d4875d2bfa835e1dda
 class Artist 
   
   attr_accessor :name, :songs  
@@ -13,6 +18,7 @@ class Artist
     @@all
   end
   
+<<<<<<< HEAD
   def self.find(name)
     self.all.find do |existing_name| 
       existing_name.name == name
@@ -27,6 +33,31 @@ end
   
   def self.find_or_create_by_name(name)
    self.find(name) || self.create(name)
+=======
+  def self.find_by_name(name)
+    @@all.each do |existing_name| 
+      if existing_name.name == name
+        existing_name
+      end
+    end
+end
+
+def self.create_by_name(name)
+  binding.pry
+   artist = self.new(name)
+     @@all << artist 
+      artist
+end
+  
+  
+  def self.find_or_create_by_name(name)
+    binding.pry
+   if self.find_by_name(name)
+     self.find_by_name(name)
+    else 
+     self.create_by_name(name)
+     end
+>>>>>>> fa915f8838ddf6833bf0c3d4875d2bfa835e1dda
   end
   
   def add_song(song)
